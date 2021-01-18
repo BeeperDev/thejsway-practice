5bf0d7d8666e1f03cf148221423822937aba5517
Erica Layton <e.alexislayton@gmail.com>
2021-01-17 17:28:24 -0800

    add practice problems from mdn/Promises

---
+++ mdnPromises.js
@@ -0,0 +1,30 @@
+const fetch1Promise = fetch('https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json');
+
+console.log(fetchPromise);
+
+fetch1Promise.then( response => {
+  console.log(`Received response: ${response.status}`);
+});
+
+console.log("Started request...");
+
+// THIS is what is returned >>
+// Promise { <state>: "pending" }
+// Started request...
+// Received response: 200
+
+
+const fetch2Promise = fetch('https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json');
+
+fetch2Promise.then( response => {
+  const jsonPromise = response.json();
+  jsonPromise.then( data => {
+    console.log(data[0].name);
+  });
+});
+
+// This is what is returned >>
+// Promise { <state>: "pending" }
+// "baked beans"
+
+
